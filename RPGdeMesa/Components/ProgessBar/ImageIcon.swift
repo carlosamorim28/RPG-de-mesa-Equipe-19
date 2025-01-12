cf0f4727aef410ad8c1a8ce713d254f32d180433
iredefbmac_22 <iredefbmac_22@Mac-mini-de-iredefbmac-22.local>
2025-01-12 17:14:56 -0300

    feat: adicionar barra de progesso

---
+++ RPGdeMesa/Components/ProgessBar/ImageIcon.swift
@@ -0,0 +1,25 @@
+//
+//  ImageIcon.swift
+//  RPGdeMesa
+//
+//  Created by iredefbmac_22 on 12/01/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var imageIcon: String
+    var body: some View {
+        ZStack{
+            Circle()
+                .frame(width: 40.0, height: 40.0)
+                .foregroundColor(.rpgLightGray)
+            Image(systemName: imageIcon)
+                .frame(width: 20.0, height: 20.0)
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView(imageIcon: "paintpalette")
+}
---
+++ RPGdeMesa/Components/ProgessBar/ProgressBar.swift
@@ -0,0 +1,30 @@
+//
+//  IconBar.swift
+//  RPGdeMesa
+//
+//  Created by iredefbmac_22 on 12/01/25.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        ZStack{
+            Color(.rpgLightGray)
+                .clipShape(Rectangle())
+                .frame(width: 430.0, height: 8.0)
+            HStack(spacing: 25){
+                SwiftUIView(imageIcon: "cursorarrow.click")
+                SwiftUIView(imageIcon: "paintpalette")
+                SwiftUIView(imageIcon: "pawprint")
+                SwiftUIView(imageIcon: "figure.arms.open")
+                SwiftUIView(imageIcon: "square.and.pencil")
+                SwiftUIView(imageIcon: "doc")
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
