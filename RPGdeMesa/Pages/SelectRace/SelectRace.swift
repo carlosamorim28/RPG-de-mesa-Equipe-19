5a2af11c9203939b7a944b5cf0fc88cd40988fa0
iredefbmac_22 <iredefbmac_22@Mac-mini-de-iredefbmac-22.local>
2025-01-11 21:47:00 -0300

    feat: finalizar tela de seleção de raça

--- RPGdeMesa/Pages/SelectRace/SelectRace.swift
+++ RPGdeMesa/Pages/SelectRace/SelectRace.swift
@@ -11,6 +11,7 @@ struct SelectRace: View {
     var body: some View {
         ScrollView(.vertical) {
             VStack(spacing: 16) {
+                ForEach(0..<1) {index in
                     HStack{
                         SelectRaceButton(title: "Humano", click: {})
                         Spacer().frame(width: 16)
@@ -23,23 +24,29 @@ struct SelectRace: View {
                         SelectRaceButton(title: "Gnomos", click: {})
                     }
                 
-                HStack{
-                    SelectRaceButton(title: "Halflings", click: {})
-                    Spacer().frame(width: 16)
-                    SelectRaceButton(title: "Draconatos", click: {})
-                }
+                    HStack{
+                        SelectRaceButton(title: "Halflings", click: {})
+                        Spacer().frame(width: 16)
+                        SelectRaceButton(title: "Draconatos",  click: {})
+                    }
                 
-                HStack{
-                    SelectRaceButton(title: "Tieflings", click: {})
-                    Spacer().frame(width: 16)
-                    SelectRaceButton(title: "Meio-Orcs", click: {})
-                }
-           }.padding(16)
-       }
-        
-        Spacer()
-            .frame(height: 24)
-        RPGNextButton()
+                    HStack{
+                        SelectRaceButton(title: "Tieflings", click: {})
+                        Spacer().frame(width: 16)
+                        SelectRaceButton(title: "Meio-Orcs", click: {})
+                    }
+               }
+                    
+          }
+        }.frame(height: 470)
+        NavigationLink {
+            //Proxima tela
+            
+        } label: {
+            RPGNextButton()
+                .padding(.top, 45.0)
+        }
+
     }
 }
 #Preview {
